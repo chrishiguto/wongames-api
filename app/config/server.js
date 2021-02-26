ec9eed4e66bb598939203059f0eb0de22b16fdc4
Christiano Higuto <chrisshiguto@gmail.com>
2021-02-26 16:17:46 -0300

    first commit - :sparkles:

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fb81d2b77616d7bdf2bc6da906c32317'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a7481235-a421-43d2-8d02-7fbf2a8b0e2e'
+};
